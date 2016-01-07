49c3350fc61a0861fa37db8ffb27bf8235155734
Andrzej Dziobek <andrzej-dziobek@wp.pl>
2016-01-07 10:12:50 +0100

    Dodanie liczby dostępnych access pointów, nr. kanału

--- src/lan/wifianalyzer/activities/ScanningActivity.java
+++ src/lan/wifianalyzer/activities/ScanningActivity.java
@@ -8,6 +8,8 @@ import java.util.TimerTask;
 
 import lan.wifianalyzer.R;
 import lan.wifianalyzer.R.layout;
+import lan.wifianalyzer.Utils;
+
 import android.app.Activity;
 import android.content.Context;
 import android.net.wifi.ScanResult;
@@ -16,84 +18,155 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class ScanningActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.scanning_activity);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scanning_activity);
 
-		final ListView listview = (ListView) findViewById(R.id.listview);
-		ArrayList<ScanResult> results = (ArrayList<ScanResult>) MainActivity.wifi
-				.getScanResults();
+        final ListView listview = (ListView) findViewById(R.id.listview);
+        ArrayList<ScanResult> results = (ArrayList<ScanResult>) MainActivity.wifi
+                .getScanResults();
 
-		final WifiNetworksAdapter adapter = new WifiNetworksAdapter();
-		adapter.setWifiNetworks(results);
-		listview.setAdapter(adapter);
+        final WifiNetworksAdapter adapter = new WifiNetworksAdapter();
+        //Uzupełniamy widok w informacje o access pointach
+        adapter.setWifiNetworks(results);
+        listview.setAdapter(adapter);
 
-		Timer myTimer = new Timer();
-		myTimer.schedule(new TimerTask() {
-			@Override
-			public void run() {
-				ScanningActivity.this.runOnUiThread(new Runnable() {
-					public void run() {
-						adapter.setWifiNetworks(MainActivity.wifi.getScanResults());
-						adapter.notifyDataSetChanged();
-					}
-				});
-			}
-		}, 1000, 1000); // initial delay 1 second, interval 1 second
+        Timer myTimer = new Timer();
 
-	}
 
-	public class WifiNetworksAdapter extends BaseAdapter {
+        //Uaktualniamy informację o access poitach w watku co sekundę
+        myTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                ScanningActivity.this.runOnUiThread(new Runnable() {
+                    public void run() {
+                        adapter.setWifiNetworks(MainActivity.wifi.getScanResults());
+                        adapter.notifyDataSetChanged();
+                        TextView t = (TextView) findViewById(R.id.number_of_networks);
+                        t.setText(String.valueOf(adapter.getCount()));
+                    }
+                });
+            }
+        }, 1000, 1000); // initial delay 1 second, interval 1 second
 
-		List<ScanResult> wifiNetworks;
+    }
 
-		public void setWifiNetworks(List<ScanResult> results) {
-			wifiNetworks = results;
-		}
 
-		@Override
-		public int getCount() {
-			return wifiNetworks.size();
-		}
+    public class WifiNetworksAdapter extends BaseAdapter {
 
-		@Override
-		public ScanResult getItem(int index) {
-			return wifiNetworks.get(index);
-		}
+        List<ScanResult> wifiNetworks;
 
-		@Override
-		public long getItemId(int arg0) {
-			return arg0;
-		}
+        public void setWifiNetworks(List<ScanResult> results) {
+            wifiNetworks = results;
+        }
 
-		@Override
-		public View getView(int arg0, View arg1, ViewGroup arg2) {
+        @Override
+        public int getCount() {
+            return wifiNetworks.size();
+        }
 
-			if (arg1 == null) {
-				LayoutInflater inflater = (LayoutInflater) ScanningActivity.this
-						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-				arg1 = inflater.inflate(R.layout.row_layout, arg2, false);
-			}
+        @Override
+        public ScanResult getItem(int index) {
+            return wifiNetworks.get(index);
+        }
 
-			TextView networkSSID = (TextView) arg1.findViewById(R.id.ssid);
-			TextView networkLevel = (TextView) arg1.findViewById(R.id.level);
+        @Override
+        public long getItemId(int arg0) {
+            return arg0;
+        }
 
-			ScanResult result = wifiNetworks.get(arg0);
+        @Override
+        public View getView(int arg0, View arg1, ViewGroup arg2) {
 
-			networkSSID.setText(result.SSID);
-			networkLevel.setText("Power level: " + result.level + " dBm");
+            if (arg1 == null) {
+                /**
+                 * Wczytujemy widok row_layout z xmla do programu
+                 */
+                LayoutInflater inflater = (LayoutInflater) ScanningActivity.this
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                arg1 = inflater.inflate(R.layout.row_layout, arg2, false);
+            }
 
-			return arg1;
-		}
+            TextView networkSSID = (TextView) arg1.findViewById(R.id.ssid);
+            TextView networkLevel = (TextView) arg1.findViewById(R.id.level);
+            TextView networkChannel = (TextView) arg1.findViewById(R.id.channel_number);
 
-	}
+            ScanResult result = wifiNetworks.get(arg0);
+            int c = computeChannel(result.frequency);
+            /**
+             * Uzupełniamy widok o informacje o access poincie
+             */
+            networkSSID.setText(result.SSID + " (" + result.BSSID + ")");
+            networkLevel.setText("Power level: " + result.level + " dBm");
+            networkChannel.setText(String.valueOf(c) + "    "+result.frequency + " MHz");
+            return arg1;
+        }
+
+        public int computeChannel(int frequency){
+            int channel;
+            switch(frequency){
+                case 2412 :
+                    channel = 1;
+                    break;
+                case 2417 :
+                    channel = 2;
+                    break;
+                case 2422 :
+                    channel = 3;
+                    break;
+                case 2427 :
+                    channel = 4;
+                    break;
+                case 2432 :
+                    channel = 5;
+                    break;
+                case 2437 :
+                    channel = 6;
+                    break;
+                case 2442 :
+                    channel = 7;
+                    break;
+                case 2447 :
+                    channel = 8;
+                    break;
+                case 2452 :
+                    channel = 9;
+                    break;
+                case 2457 :
+                    channel = 10;
+                    break;
+                case 2462 :
+                    channel = 11;
+                    break;
+                case 2467 :
+                    channel = 12;
+                    break;
+                case 2472 :
+                    channel = 13;
+                    break;
+                case 2477 :
+                    channel = 14;
+                    break;
+                case 2482 :
+                    channel = 15;
+                    break;
+                default :
+                    channel = 0;
+                    break;
+            }
+            return channel;
+        }
+
+    }
+
 
 }
